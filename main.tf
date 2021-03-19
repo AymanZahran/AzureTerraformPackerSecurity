c816c70b06fb81d7efea342e6045fbbf3470f19a
Ayman Zahran <ah.zahran@outlook.com>
2021-03-19 22:06:59 +0200

    Securing Keys and Modify .gitignore to hide .tfvars

--- main.tf
+++ main.tf
@@ -12,15 +12,6 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
-  # More information on the authentication methods supported by
-  # the AzureRM Provider can be found here:
-  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
-
-  subscription_id = "8f1f9ef1-522d-49f2-b4c5-5cf775f1769a"
-  client_id       = "087b41c2-003d-42ab-84fd-65eb48dddf8a"
-  #client_secret   = "5476149f-0805-4197-8e66-4b7c2fb42f2e"
-  tenant_id       = "b045b6bd-eac0-4f42-ac93-f285f9d7ffe6"
 }
 
 # Create a resource group
