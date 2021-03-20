59534ec915da22ec70dd7409a654b6a5fe6595ee
Ayman Zahran <ah.zahran@outlook.com>
2021-03-20 14:06:53 +0200

    Modifying packer

---
+++ setENV.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export AZURE_SUBSCRIPTION_ID="8f1f9ef1-522d-49f2-b4c5-5cf775f1769a"
+export AZURE_TENANT_ID="b045b6bd-eac0-4f42-ac93-f285f9d7ffe6"
+export CLIENT_ID="087b41c2-003d-42ab-84fd-65eb48dddf8a"
+export CLIENT_SECRET="5476149f-0805-4197-8e66-4b7c2fb42f2e"
