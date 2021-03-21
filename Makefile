7e390a5d09c13a151cc97aee32bc7b9fc3dba82f
Ayman Zahran <ah.zahran@outlook.com>
2021-03-21 21:51:49 +0200

    Securing Packer Variables

--- Makefile
+++ Makefile
@@ -2,12 +2,17 @@ Hello:
 	echo "Begining the Udacity Deploying Web Server on Azure using Azure Policy, Packer and Terraform"
 Policy:
 	echo "Building Azure Policy"
+	cd Policies
 	az policy definition create --name Tagging-Policy --display-name "Tagging-Policy" --description "Enforce Tagging on all Resources" --rules EnforceTags.json --mode All	
+	cd ..
 Packer:
 	echo "Building Images"
-	packer build -var-file=".tfvars" server.json
-	
+	cd Packer
+	packer build -var-file=".pkrvars" server.json
+	cd ..
 Terraform:
 	echo "Building Infrastructure"
+	cd Terraform
 	terraform apply -var-file=".tfvars"
+	cd ..
 all: Policy Packer Terraform
---
+++ Terraform/main.tf
@@ -0,0 +1,29 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "production-resources"
+  location = "West US"
+}
+
+# Create a virtual network in the production-resources resource group
+resource "azurerm_virtual_network" "test" {
+  name                = "production-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
