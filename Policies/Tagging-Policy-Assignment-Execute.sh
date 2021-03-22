ac49c8b514133aa5ab08eef813e178dc4e9710bb
Ayman Zahran <ah.zahran@outlook.com>
2021-03-22 21:02:48 +0200

    Modify Terraform Variables

--- Policies/Tagging-Policy-Assignment-Execute.sh
+++ Policies/Tagging-Policy-Assignment-Execute.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-az policy assignment create --name 'Tagging-Policy-Assignment' --display-name 'Tagging-Policy-Assignement' --params Policies/Tagging-Policy-Assignment-Params.json --scope /subscriptions/`echo $AZURE_SUBSCRIPTION_ID` --policy /subscriptions/`echo $AZURE_SUBSCRIPTION_ID`/providers/Microsoft.Authorization/policyDefinitions/Tagging-Policy
+az policy assignment create --name 'Tagging-Policy-Assignment' --display-name 'Tagging-Policy-Assignement' --params Tagging-Policy-Assignment-Params.json --scope /subscriptions/`echo $AZURE_SUBSCRIPTION_ID` --policy /subscriptions/`echo $AZURE_SUBSCRIPTION_ID`/providers/Microsoft.Authorization/policyDefinitions/Tagging-Policy
--- Policies/Tagging-Policy-Definition-Execute.sh
+++ Policies/Tagging-Policy-Definition-Execute.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-az policy definition create --name 'Tagging-Policy' --display-name 'Tagging-Policy' --description 'Enforce Tagging on all Resources' --params Policies/Tagging-Policy-Definition-Params.json --rules Policies/Tagging-Policy-Definition-Rules.json  --subscription `echo $AZURE_SUBSCRIPTION_ID` --mode All
+az policy definition create --name 'Tagging-Policy' --display-name 'Tagging-Policy' --description 'Enforce Tagging on all Resources' --params Tagging-Policy-Definition-Params.json --rules Tagging-Policy-Definition-Rules.json  --subscription `echo $AZURE_SUBSCRIPTION_ID` --mode All
