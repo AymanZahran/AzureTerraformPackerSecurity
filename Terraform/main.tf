dfb3b87be3ad9b9ca4d2643537f06288491fb8ac
Ayman Zahran <ah.zahran@outlook.com>
2021-03-23 20:42:39 +0200

    Adding vNICs, LBs and ASs

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,7 +2,6 @@
 resource "azurerm_resource_group" "RG-WebServers" {
   name     = "production-resources"
   location = var.AZURE_LOCATION
-
   tags = {
     webserver-env = "Production"
   }
@@ -14,14 +13,88 @@ resource "azurerm_virtual_network" "VNET-WebServers" {
   resource_group_name = azurerm_resource_group.RG-WebServers.name
   location            = azurerm_resource_group.RG-WebServers.location
   address_space       = ["10.0.0.0/16"]
-
-  subnet {
-    name           = "Subnet-FrontEnd"
-    address_prefix = "10.0.0.0/24"
-  }
-
   tags = {
     webserver-env = "Production"
+  }
+}
+# Create a Subnet in the production-resources resource group
+resource "azurerm_subnet" "Subnet-WebServers" {
+  name                 = "Subnet-WebServers"
+  resource_group_name  = azurerm_resource_group.RG-WebServers.name
+  virtual_network_name = azurerm_virtual_network.VNET-WebServers.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+# Create a Public IPs in the production-resources resource group
+resource "azurerm_public_ip" "PublicIP-WebServer1" {
+  name                = "PublicIP-WebServer1"
+  resource_group_name = azurerm_resource_group.RG-WebServers.name
+  location            = azurerm_resource_group.RG-WebServers.location
+  allocation_method   = "Static"
+  tags = {
+    webserver-env = "Production"
+  }
+}
+resource "azurerm_public_ip" "PublicIP-WebServer2" {
+  name                = "PublicIP-WebServer2"
+  resource_group_name = azurerm_resource_group.RG-WebServers.name
+  location            = azurerm_resource_group.RG-WebServers.location
+  allocation_method   = "Static"
+  tags = {
+    webserver-env = "Production"
+  }
+}
+resource "azurerm_public_ip" "PublicIP-WebServer3" {
+  name                = "PublicIP-WebServer3"
+  resource_group_name = azurerm_resource_group.RG-WebServers.name
+  location            = azurerm_resource_group.RG-WebServers.location
+  allocation_method   = "Static"
+  tags = {
+    webserver-env = "Production"
+  }
+}
+
+# Create a Network Interfaces in the production-resources resource group
+resource "azurerm_network_interface" "VNIC-WebServer1" {
+  name                = "VNIC-WebServer1"
+  location            = azurerm_resource_group.RG-WebServers.location
+  resource_group_name = azurerm_resource_group.RG-WebServers.name
+  ip_configuration {
+    name                          = "IPconfig1"
+    subnet_id                     = azurerm_subnet.Subnet-WebServers.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.PublicIP-WebServer1.id
+  }
+  tags = {
+    webserver-env = "Production"
+  }
+}
+resource "azurerm_network_interface" "VNIC-WebServer2" {
+  name                = "VNIC-WebServer2"
+  location            = azurerm_resource_group.RG-WebServers.location
+  resource_group_name = azurerm_resource_group.RG-WebServers.name
+  ip_configuration {
+    name                          = "IPconfig2"
+    subnet_id                     = azurerm_subnet.Subnet-WebServers.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.PublicIP-WebServer2.id
+  }
+  tags = {
+    webserver-env = "Production"
+  }
+}
+resource "azurerm_network_interface" "VNIC-WebServer3" {
+  name                = "VNIC-WebServer3"
+  location            = azurerm_resource_group.RG-WebServers.location
+  resource_group_name = azurerm_resource_group.RG-WebServers.name
+  tags = {
+    webserver-env = "Production"
+  }
+  ip_configuration {
+    name                          = "IPconfig3"
+    subnet_id                     = azurerm_subnet.Subnet-WebServers.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.PublicIP-WebServer3.id
   }
 }
 
@@ -30,12 +103,10 @@ resource "azurerm_network_security_group" "NSG-WebServers" {
   name                = "production-nsgs"
   location            = azurerm_resource_group.RG-WebServers.location
   resource_group_name = azurerm_resource_group.RG-WebServers.name
-  
   tags = {
     webserver-env = "Production"
   }
 }
-
 resource "azurerm_network_security_rule" "AllowLocalInbound" {
     name                       = "AllowLocalInbound"
     priority                   = 100
@@ -63,6 +134,78 @@ resource "azurerm_network_security_rule" "AllowLocalOutbound" {
     network_security_group_name = azurerm_network_security_group.NSG-WebServers.name
   }
 
+# Associate Network Interfaces to Network Security Groups
+resource "azurerm_network_interface_security_group_association" "VNIC-NSG-1" {
+  network_interface_id      = azurerm_network_interface.VNIC-WebServer1.id
+  network_security_group_id = azurerm_network_security_group.NSG-WebServers.id
+}
+resource "azurerm_network_interface_security_group_association" "VNIC-NSG-2" {
+  network_interface_id      = azurerm_network_interface.VNIC-WebServer2.id
+  network_security_group_id = azurerm_network_security_group.NSG-WebServers.id
+}
+resource "azurerm_network_interface_security_group_association" "VNIC-NSG-3" {
+  network_interface_id      = azurerm_network_interface.VNIC-WebServer3.id
+  network_security_group_id = azurerm_network_security_group.NSG-WebServers.id
+}
+
+#Creating a Public IP for the Load Balancer in the production-resource resource group
+resource "azurerm_public_ip" "PublicIP-LB" {
+  name                = "PublicIP-LB"
+  location            = azurerm_resource_group.RG-WebServers.location
+  resource_group_name = azurerm_resource_group.RG-WebServers.name
+  allocation_method   = "Static"
+  tags = {
+    webserver-env = "Production"
+  }
+}
+
+#Create LoadBalancer
+resource "azurerm_lb" "LB-WebServers" {
+  name                = "LB-WebServers"
+  location            = azurerm_resource_group.RG-WebServers.location
+  resource_group_name = azurerm_resource_group.RG-WebServers.name
+
+  frontend_ip_configuration {
+    name                 = "Frontend-IPconfig"
+    public_ip_address_id = azurerm_public_ip.PublicIP-LB.id
+  }
+  tags = {
+    webserver-env = "Production"
+  }
+}
+
+#Create LoadBalancer Backend Address Pool
+resource "azurerm_lb_backend_address_pool" "BackEndAddressPool" {
+  loadbalancer_id = azurerm_lb.LB-WebServers.id
+  name            = "BackEndAddressPool"
+}
+
+#Associate WebServer NICs to LoadBalancer BackEnd Pool
+resource "azurerm_network_interface_backend_address_pool_association" "LB-VNIC-WebServer1" {
+  network_interface_id    = azurerm_network_interface.VNIC-WebServer1.id
+  ip_configuration_name   = "IPconfig1"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.BackEndAddressPool.id
+}
+resource "azurerm_network_interface_backend_address_pool_association" "LB-VNIC-WebServer2" {
+  network_interface_id    = azurerm_network_interface.VNIC-WebServer2.id
+  ip_configuration_name   = "IPconfig2"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.BackEndAddressPool.id
+}
+resource "azurerm_network_interface_backend_address_pool_association" "LB-VNIC-WebServer3" {
+  network_interface_id    = azurerm_network_interface.VNIC-WebServer3.id
+  ip_configuration_name   = "IPconfig3"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.BackEndAddressPool.id
+}
+
+# Create a WebServer Availability Set in the production-resources resource group
+resource "azurerm_availability_set" "AS-WebServers" {
+  name                = "AS-WebServers"
+  location            = azurerm_resource_group.RG-WebServers.location
+  resource_group_name = azurerm_resource_group.RG-WebServers.name
+  tags = {
+    webserver-env = "Production"
+  }
+}
 
 
 
